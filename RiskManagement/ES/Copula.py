7319a55836fad01ef525be5d52e5bfacc1d2a97c
Xianqi Dong <xianqi.dong@duke.edu>
2024-03-04 10:20:29 -0500

    Week05 Assignment-03/04/2024

--- RiskManagement/ES/Copula.py
+++ RiskManagement/ES/Copula.py
@@ -7,8 +7,7 @@ sys.path.append( '../VaR' )
 import VaR as v
 sys.path.append( '../Sim' )
 import Sim as s
-sys.path.append( '../ES' )
-import ES as es
+
 
 def copula_risk(portfolio, ror, nSim = 100000):
     portfolio["currentValue"] = portfolio["Holding"]  * portfolio["Starting Price"]
@@ -42,28 +41,31 @@ def copula_risk(portfolio, ror, nSim = 100000):
             simRet[stock] = stats.t.ppf(uSim[stock], df = nu[stock], loc = mu[stock], scale = sigma[stock])
     # simulatedValue = portfolio["currentValue"] * (1 + simRet)
     pnl = portfolio["currentValue"] * simRet
+    pnl["Total"] = 0
     risk = pd.DataFrame(columns = ["Stock", "VaR95", "ES95", "VaR95_Pct", "ES95_Pct"])
-    for stock in pnl.columns:
+    for stock in pnl.columns[:-1]:
+        pnl["Total"] += pnl[stock]
         i = risk.shape[0]
         risk.loc[i, "Stock"] = stock
         risk.loc[i, "VaR95"] = -np.percentile(pnl[stock], 5)
         risk.loc[i, "VaR95_Pct"] = risk.loc[i, "VaR95"] / portfolio.loc[stock, "currentValue"]
-        ub = -risk.loc[i, "VaR95_Pct"]
-        if portfolio.loc[stock, "Distribution"] == "Normal":
-            risk.loc[i, "ES95_Pct"] = -stats.norm.expect(ub = ub, loc = mu[stock], scale = sigma[stock], conditional = True)
-        elif portfolio.loc[stock, "Distribution"] == "T":
-            risk.loc[i, "ES95_Pct"] = -stats.t.expect(ub = ub, args=(nu[stock],), loc = mu[stock], scale = sigma[stock], conditional = True)
-        risk.loc[i, "ES95"] = risk.loc[i, "ES95_Pct"] * portfolio.loc[stock, "currentValue"]
+        ub = -risk.loc[i, "VaR95"]
+        # if portfolio.loc[stock, "Distribution"] == "Normal":
+        #     risk.loc[i, "ES95_Pct"] = -stats.norm.expect(ub = ub, loc = mu[stock], scale = sigma[stock], conditional = True)
+        # elif portfolio.loc[stock, "Distribution"] == "T":
+        #     risk.loc[i, "ES95_Pct"] = -stats.t.expect(ub = ub, args=(nu[stock],), loc = mu[stock], scale = sigma[stock], conditional = True)
+        risk.loc[i, "ES95"] = -np.mean(pnl[pnl[stock] <= ub][stock])
+        risk.loc[i, "ES95_Pct"] = risk.loc[i, "ES95"] / portfolio.loc[stock, "currentValue"]
     total_value = sum(portfolio["currentValue"])
-    W = portfolio["currentValue"] / total_value
-    total_mu = np.mean(np.sum(W * simRet, axis = 1))
-    total_sigma = np.sqrt(W.T @ simRet.cov() @ W)
-    total_VaR = -stats.norm.ppf(0.05, loc = total_mu, scale = total_sigma)
+    # W = portfolio["currentValue"] / total_value
+    # total_mu = np.mean(np.sum(W * simRet, axis = 1))
+    # total_sigma = np.sqrt(W.T @ simRet.cov() @ W)
+    total_VaR = -np.percentile(pnl["Total"], 5)
     row_total = risk.shape[0]
     risk.loc[row_total, "Stock"] = "Total"
-    risk.loc[row_total, "VaR95"] = total_VaR * total_value
-    risk.loc[row_total, "VaR95_Pct"] = total_VaR
-    ub = -risk.loc[row_total, "VaR95_Pct"]
-    risk.loc[row_total, "ES95_Pct"] = -stats.norm.expect(ub = ub, loc = total_mu, scale = total_sigma, conditional = True)
-    risk.loc[row_total, "ES95"] = risk.loc[row_total, "ES95_Pct"] * total_value
+    risk.loc[row_total, "VaR95"] = total_VaR
+    risk.loc[row_total, "VaR95_Pct"] = total_VaR / total_value
+    ub = -risk.loc[row_total, "VaR95"]
+    risk.loc[row_total, "ES95"] = -np.mean(pnl[pnl["Total"] <= ub]["Total"])
+    risk.loc[row_total, "ES95_Pct"] = risk.loc[row_total, "ES95"] / total_value
     return risk
